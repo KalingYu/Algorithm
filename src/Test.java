00e035a1d5a6127ea52de8d0fc4bb682d8e88abd
Kaling <yujialingbbs@163.com>
2015-06-06 15:20:32 +0800

    算法学习

---
+++ src/Test.java
@@ -0,0 +1,28 @@
+
+import java.io.BufferedInputStream;
+import java.util.Scanner;
+
+public class Test {
+
+
+    public static void main(String[] args) {
+        int [] numArray = new int[100];
+        int sum = 0;
+        int bigger = 0;
+        Scanner scanner = new Scanner(new BufferedInputStream(System.in));
+        int n, m;
+        n = scanner.nextInt();
+        for (int i = 0; i < n; i++) {
+            numArray[i] = scanner.nextInt();
+            sum++;
+        }
+        m = scanner.nextInt();
+        for (int i = 0; i < sum; i++) {
+            if (m > numArray[i]) {
+                bigger++;
+            }
+        }
+
+        System.out.println(bigger);
+    }
+}
